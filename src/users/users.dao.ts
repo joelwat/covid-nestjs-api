d216c29a97041d126c2ecbaf588e29ac0f541713
Joel Watson <joelwat@gmail.com>
2021-01-31 13:29:48 -0800

    Adds stubbed user dao

---
+++ src/users/users.dao.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon';
+
+import { User } from 'src/entities';
+
+export class UserDAO {
+  static find(/* id: number */): User {
+    return new User({
+      id: 1,
+      email: 'foo@bar.com',
+      createdOn: DateTime.utc(),
+      updatedOn: DateTime.utc(),
+    });
+  }
+
+  static findAll(): User[] {
+    return [
+      new User({
+        id: 1,
+        email: 'foo@bar.com',
+        createdOn: DateTime.utc(),
+        updatedOn: DateTime.utc(),
+      }),
+    ];
+  }
+}
