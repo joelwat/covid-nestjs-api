25561c0818e8b0a0dca9d465b0f56c12c4e9d780
Joel Watson <joelwat@gmail.com>
2021-01-31 21:08:35 -0800

    Changes linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+      }
+    ],
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
   },
   settings: {
     'import/resolver': {
